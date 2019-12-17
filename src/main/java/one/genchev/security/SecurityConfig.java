0c48c08ed0e7396036753fc026f0a801c305a8cc
Orlin Genchev <ogenchev@gmail.com>
2019-12-17 12:31:01 +0200

    add h2-db user account persistence

--- src/main/java/one/genchev/security/SecurityConfig.java
+++ src/main/java/one/genchev/security/SecurityConfig.java
@@ -1,11 +1,17 @@
 package one.genchev.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -19,13 +25,48 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //
 //    }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth)
+    @Autowired
+    DataSource dataSource;
+
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth)
+//            throws Exception {
+//
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource);
+//    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
             throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("test")
-                .password("{noop}test123")
-                .roles("USER");
-//                .authorities("ROLE_USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(User.withUsername("user")
+                        .password(passwordEncoder().encode("pass"))
+                        .roles("USER"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity)
+            throws Exception {
+//        httpSecurity.authorizeRequests()
+//                .antMatchers("/h2-console/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin();
+
+        httpSecurity.csrf()
+                .ignoringAntMatchers("/h2-console/**");
+        httpSecurity.headers()
+                .frameOptions()
+                .sameOrigin();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
